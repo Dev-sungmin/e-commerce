02e880d6e4913e305155a4e03a729dfb7881a7e4
Dev-sungmin <sungminmail0@naver.com>
2026-07-29 13:54:18 +0900

    feat: 인증 화면 라우팅 연결 (공개/보호 경로 분리)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
-
-// TODO: react-router-dom BrowserRouter/Routes로 교체
-// 예: /login, /products, /orders, /admin/inventory 등
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './features/user/context/AuthContext';
+import LoginPage from './features/user/pages/LoginPage';
+import SignupPage from './features/user/pages/SignupPage';
+import AuthCheckPage from './features/user/pages/AuthCheckPage';
 
 function App() {
-  return <div>TODO: 라우팅 구성</div>;
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<AuthCheckPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignupPage />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
+    );
 }
 
 export default App;
