389ca502667f0504eab4c311bc3b266ed2eac68e
Dev-sungmin <sungminmail0@naver.com>
2026-08-04 12:15:17 +0900

    feat: EC2 보안그룹을 Gateway 포트(8000)만 개방하도록 정리 (User Service/PHP 개별 포트 노출 제거)

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -56,25 +56,17 @@ resource "aws_security_group" "app_sg" {
   description = "Allow app port only (SSH access via SSM, no port 22 needed)"
 
   ingress {
-    description = "User Service - (temporary, will be removed once Gateway is set up)"
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "PHP legacy (temporary for testing, will be closed once Gateway is set up)"
-    from_port   = 8090
-    to_port     = 8090
+    description = "Gateway - single entry point for all API traffic"
+    from_port   = 8000
+    to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
