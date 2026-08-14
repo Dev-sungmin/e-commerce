907aba928a3c53d465699a6ba6ae901db757caa4
Dev-sungmin <sungminmail0@naver.com>
2026-08-14 10:50:21 +0900

    infra: frontend.tf에 버킷명/CloudFront distribution ID output 추가

--- infra/terraform/frontend.tf
+++ infra/terraform/frontend.tf
@@ -88,3 +88,11 @@ resource "aws_cloudfront_distribution" "frontend" {
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.frontend.domain_name
 }
+
+output "frontend_bucket_name" {
+  value = aws_s3_bucket.frontend.bucket
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.frontend.id
+}
